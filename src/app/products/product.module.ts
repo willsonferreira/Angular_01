98de168260205093933645b1c8daff11d515060e
willson.ferreira@outlook.com <willson.ferreira@outlook.com>
2020-04-11 22:56:01 -0300

    products lidt

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { ProductListComponent } from './product-list.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent }
-    ])
+    ]),
+    SharedModule
   ],
   declarations: [
     ProductListComponent
