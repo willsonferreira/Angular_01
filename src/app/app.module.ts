24cf97a0fbad0f1bb18ae90650239dab68da343b
willson.ferreira@outlook.com <willson.ferreira@outlook.com>
2020-04-12 14:03:01 -0300

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 
 import { AppComponent } from './app.component';
@@ -7,7 +8,8 @@ import { ProductListComponent } from './products/product-list.component'
 
 @NgModule({
   imports: [
-      BrowserModule
+      BrowserModule,
+      NgbModule
     ],
   declarations: [
     AppComponent,
