a8aa43a7c5d853b9c18243d92172464a8c16ed97
willson.ferreira@outlook.com <willson.ferreira@outlook.com>
2020-04-12 10:42:31 -0300

    undo

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,18 +1,1 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
-import { ProductListComponent } from './product-list.component';
-import { SharedModule } from '../shared/shared.module';
-
-@NgModule({
-  imports: [
-    RouterModule.forChild([
-      { path: 'products', component: ProductListComponent }
-    ]),
-    SharedModule
-  ],
-  declarations: [
-    ProductListComponent
-  ]
-})
 export class ProductModule {}
