fec0a977cc2ca20df50a37812ba337d834ae6ee8
willson.ferreira@outlook.com <willson.ferreira@outlook.com>
2020-04-11 22:57:27 -0300

    products list

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StarComponent } from './star.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
