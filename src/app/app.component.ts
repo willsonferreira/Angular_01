aae04d76d34b54c3ed76ce72cf6b809d29334702
willson.ferreira@outlook.com <willson.ferreira@outlook.com>
2020-04-12 12:18:27 -0300

    product list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: '<h1>{{pageTitle}}</h1>',
+  template: '<div><h1>{{pageTitle}}</h1><pm-products></pm-products></div>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
