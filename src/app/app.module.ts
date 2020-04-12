51323716737964a7b00d392bb7ac2fbc5768bd3c
willson.ferreira@outlook.com <willson.ferreira@outlook.com>
2020-04-12 14:09:06 -0300

    Forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component'
@@ -9,7 +9,8 @@ import { ProductListComponent } from './products/product-list.component'
 @NgModule({
   imports: [
       BrowserModule,
-      NgbModule
+      FormsModule,
+      NgbModule.forRoot()
     ],
   declarations: [
     AppComponent,
