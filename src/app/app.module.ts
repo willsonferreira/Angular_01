3a1c17f1ea7d892de7581f215744c7c4d7b3963e
willson.ferreira@outlook.com <willson.ferreira@outlook.com>
2020-04-12 11:05:15 -0300

    ubdo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    WelcomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot([
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
-    ])
+    AppComponent
   ],
   bootstrap: [AppComponent]
 })
