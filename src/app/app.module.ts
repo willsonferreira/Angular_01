a19e94e528ca3cb72c16b7b91d282e8567642674
willson.ferreira@outlook.com <willson.ferreira@outlook.com>
2020-04-13 16:42:29 -0300

    fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
