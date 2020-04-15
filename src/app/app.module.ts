d347f7c24c005e6d85d1781dccc2720ee80b1424
Willson Luis Ferreira <willson.ferreira@outlook.com>
2020-04-15 11:01:06 -0300

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser'
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component'
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
-  imports: [
-      BrowserModule,
-      FormsModule,
-      NgbModule,
-      FontAwesomeModule
-    ],
   declarations: [
     AppComponent,
     ProductListComponent
   ],
+  imports: [
+      BrowserModule,
+      FormsModule
+    ],  
+  providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule {
 }
