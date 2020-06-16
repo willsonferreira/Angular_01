36ef351c08173cd458acd5cefeabb9a9609e5398
willsonferreira <13160165+willsonferreira@users.noreply.github.com>
2020-06-16 18:58:39 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(`${__dirname}/dist/`));
  
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist//index.html`));
+res.sendFile(path.join(`${__dirname}/dist/index.html`));
 });
  
 app.listen(process.env.PORT || 8080);
